89129d946f8ce5e98156c22fb54b0e9779ae03d5
Ian Hudson <ian@cel-uk.com>
2015-10-11 14:08:52 +0100

    Initial commit

---
+++ src/main/java/celtech/roboxremote/RoboxRemote.java
@@ -0,0 +1,48 @@
+package celtech.roboxremote;
+
+import celtech.comms.DiscoveryAgentRemoteEnd;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+/**
+ *
+ * @author Ian
+ */
+public class RoboxRemote extends Application<RoboxRemoteConfiguration>
+{
+
+    public static void main(String[] args) throws Exception
+    {
+        new RoboxRemote().run(args);
+    }
+
+    @Override
+    public String getName()
+    {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<RoboxRemoteConfiguration> bootstrap)
+    {
+       DiscoveryAgentRemoteEnd discoveryAgent = new DiscoveryAgentRemoteEnd();
+       
+       Thread discoveryThread = new Thread(discoveryAgent);
+       discoveryThread.start();
+    }
+
+    @Override
+    public void run(RoboxRemoteConfiguration configuration,
+            Environment environment)
+    {
+        final RoboxRemoteResource resource = new RoboxRemoteResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck
+                = new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+}
