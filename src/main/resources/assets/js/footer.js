00cc7fefa639b0d6af6f73d9c0750d5f792e8570
Ian Hudson <ian@cel-uk.com>
2017-03-31 09:31:32 +0100

    Added reprint subtitle. Prevent drag of images. Suppress language selector. Add cache call for printer ID

--- src/main/resources/assets/js/footer.js
+++ src/main/resources/assets/js/footer.js
@@ -1,5 +1,5 @@
 document.write(
-        "<div class='footer-box' draggable='false'>" +
+        "<div class='footer-box'>" +
         "<span class='text-center server-name-title footer-display' style='flex-grow: 1;'>No Server</span>" +
         "<a class='btn footer-btn' href='printerStatus.html'><img class='img-fluid root-icon' alt='Home' src='robox-images/home_symbol.png'></a>" +
         "<a class='btn footer-btn' href='serverStatus.html'><img class='img-fluid root-icon' altroot-icon='Setup' src='robox-images/setup.png' height='50'></a>" +
