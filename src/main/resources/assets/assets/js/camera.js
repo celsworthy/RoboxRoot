ded6bb251d3b1bc5dcf610055437008546d74d7b
gingemonster <brian@earthware.co.uk>
2023-01-01 15:33:37 +0000

    adding in support to promisePostCommandToRoot for reading binary files for the camera images

--- src/main/resources/assets/assets/js/camera.js
+++ src/main/resources/assets/assets/js/camera.js
@@ -1,6 +1,21 @@
+var cameraprofilejson = {"profile":{"profileName":"Default","captureHeight":720,"captureWidth":1080,"headLightOff":true,"ambientLightOff":false,"moveBeforeCapture":true,"moveToX":50,"moveToY":50,"cameraName":"","controlSettings":{}},"camera":{"udevName":"/dev/video0","cameraName":"camera0","cameraNumber":0,"serverIP":""}};
+
 function cameraInit()
 {
     //$('#left-button').on('click', goToPreviousPage);
+
+    
+    promisePostCommandToRoot('cameraControl/0/snapshot', cameraprofilejson, true)
+        .then(function(result) 
+              {
+                var reader = new FileReader();
+                reader.onload = function() {                         
+                    var b64 = reader.result
+                    console.log("This is base64", b64)
+                    document.getElementById("camerapreview").src = b64
+                }
+                reader.readAsDataURL(result)
+              });    
 
     setHomeButton();
 }
