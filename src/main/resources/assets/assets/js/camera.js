e9202f1453bff0236a51ee57ba49baff29199f2b
gingemonster <brian@earthware.co.uk>
2023-01-01 16:25:56 +0000

    reading camera profiles in and supporting when no camera found

--- src/main/resources/assets/assets/js/camera.js
+++ src/main/resources/assets/assets/js/camera.js
@@ -1,21 +1,41 @@
-var cameraprofilejson = {"profile":{"profileName":"Default","captureHeight":720,"captureWidth":1080,"headLightOff":true,"ambientLightOff":false,"moveBeforeCapture":true,"moveToX":50,"moveToY":50,"cameraName":"","controlSettings":{}},"camera":{"udevName":"/dev/video0","cameraName":"camera0","cameraNumber":0,"serverIP":""}};
-
+//var cameraprofilejson = {"profile":{"profileName":"Default","captureHeight":720,"captureWidth":1080,"headLightOff":true,"ambientLightOff":false,"moveBeforeCapture":true,"moveToX":50,"moveToY":50,"cameraName":"","controlSettings":{}},"camera":{"udevName":"/dev/video0","cameraName":"camera0","cameraNumber":0,"serverIP":""}};
+var cameraprofilejson;
 function cameraInit()
 {
-    //$('#left-button').on('click', goToPreviousPage);
-
+    $('#left-button').on('click', goToPreviousPage);
     
-    promisePostCommandToRoot('cameraControl/0/snapshot', cameraprofilejson, true)
+    GetCameraProfile();
+    
+    setHomeButton();
+}
+
+function GetCameraProfile(){
+    promiseGetCommandToRoot('discovery/listCameras', null)
+        .then(function(result) 
+              {
+                if(result.cameras.length){
+                    // only supporting one for now
+                    var camera = result.cameras[0];
+                    cameraprofilejson = {"profile":{},"camera":{"udevName":camera.udevName,"cameraName":camera.cameraName,"cameraNumber":camera.cameraNumber,"serverIP":""}}
+                    setInterval(loadFrame, 1000);                    
+                }
+                else{
+                    $("#no-cameras").removeClass("hidden");
+                }
+              });   
+}
+
+function loadFrame(){
+    if(cameraprofilejson){
+        promisePostCommandToRoot('cameraControl/0/snapshot', cameraprofilejson, true)
         .then(function(result) 
               {
                 var reader = new FileReader();
                 reader.onload = function() {                         
                     var b64 = reader.result
-                    console.log("This is base64", b64)
                     document.getElementById("camerapreview").src = b64
                 }
                 reader.readAsDataURL(result)
               });    
-
-    setHomeButton();
+    }
 }
